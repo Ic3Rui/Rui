968a5595aba43bd8822aa1097c5e30835611b05a
Ic3Rui <Ic3Rui@gmx.net>
2023-11-13 22:56:11 +0100

    feat: 2.31 passing and Using Data with eventbinding

--- src/app/Tutorial/servers/servers.component.ts
+++ src/app/Tutorial/servers/servers.component.ts
@@ -6,17 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
-allowNewServer = false;
-serverCreationStatus = 'No server was created!';
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
 
-constructor() {
-  setTimeout(() => {
-    this.allowNewServer = true;
-  }, 2000);
-}
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
-onCreateServer() {
-this.serverCreationStatus = 'Server was created!';
-}
+  onCreateServer() {
+  this.serverCreationStatus = 'Server was created!';
+  }
 
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
+  }
 }
