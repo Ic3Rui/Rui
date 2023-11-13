bc106679c593b63e64483ef26866b06276838125
Ic3Rui <Ic3Rui@gmx.net>
2023-11-13 18:26:19 +0100

    feat: 2.24 Practice components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './Tutorial/server/server.component';
 import { ServersComponent } from './Tutorial/servers/servers.component';
 import { ServersInlineTemplateComponent } from './Tutorial/servers-inline-template/servers-inline-template.component';
+import { SuccessAlertComponent } from './Tutorial/Practice/success-alert/success-alert.component';
+import { WarningAlertComponent } from './Tutorial/Practice/warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    ServersInlineTemplateComponent
+    ServersInlineTemplateComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
   ],
   imports: [
     BrowserModule
