2617718ae7847a5a366d7e43be7bbb6cf671f075
Ic3Rui <Ic3Rui@gmx.net>
2023-11-13 15:49:12 +0100

    feat: 2.21 component-templates

---
+++ src/app/Tutorial/servers-inline-template/servers-inline-template.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers-inline-template',
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>`,
+  styleUrls: ['./servers-inline-template.component.css']
+})
+export class ServersInlineTemplateComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './Tutorial/server/server.component';
 import { ServersComponent } from './Tutorial/servers/servers.component';
+import { ServersInlineTemplateComponent } from './Tutorial/servers-inline-template/servers-inline-template.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
+    ServersInlineTemplateComponent
   ],
   imports: [
     BrowserModule
