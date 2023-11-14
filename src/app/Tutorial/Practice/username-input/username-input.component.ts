ed788266a56d3461d716a58c032cda69b22294e8
Ic3Rui <Ic3Rui@gmx.net>
2023-11-14 16:15:26 +0100

    feat: 2.35 Practice: Databinding with Username Inputfield

---
+++ src/app/Tutorial/Practice/username-input/username-input.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-username-input',
+  templateUrl: './username-input.component.html',
+  styleUrls: ['./username-input.component.css']
+})
+export class UsernameInputComponent {
+  username = '';
+
+  onResetUserName() {
+    this.username = ''
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ServersComponent } from './Tutorial/servers/servers.component';
 import { ServersInlineTemplateComponent } from './Tutorial/servers-inline-template/servers-inline-template.component';
 import { SuccessAlertComponent } from './Tutorial/Practice/success-alert/success-alert.component';
 import { WarningAlertComponent } from './Tutorial/Practice/warning-alert/warning-alert.component';
+import { UsernameInputComponent } from './Tutorial/Practice/username-input/username-input.component';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { WarningAlertComponent } from './Tutorial/Practice/warning-alert/warning
     ServersInlineTemplateComponent,
     SuccessAlertComponent,
     WarningAlertComponent,
+    UsernameInputComponent,
   ],
   imports: [
     BrowserModule,
